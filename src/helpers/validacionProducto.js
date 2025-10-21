eaf3bb01a83f175d4703df5c52d800ad9e6a88e6
Cristian <diazcristian0210@gmail.com>
2025-10-21 11:33:56 -0300

    Creé la validacion del precio

--- src/helpers/validacionProducto.js
+++ src/helpers/validacionProducto.js
@@ -1,17 +1,27 @@
 import { check } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
 
-const validacionProducto =  [
-      check("nombreProducto")
-        .not()
-        .notEmpty()
-        .withMessage("El nombre del producto es obligatorio")
-        .isLength({ min: 2, max: 50 })
-        .withMessage(
-          "El nombre del producto debe tener entre 2 y 50 caracteres"
-        ),
-        //Aquí llamo a  resultadoValidacion
-        (req, res, next) => resultadoValidacion(req, res, next)
-    ]
+const validacionProducto = [
+  check("nombreProducto")
+    .not()
+    .notEmpty()
+    .withMessage("El nombre del producto es obligatorio")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("El nombre del producto debe tener entre 2 y 50 caracteres"),
+  check("precio")
+    .notEmpty()
+    .withMessage("El precio del producto es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un valor numérico")
+    .custom((value) => {
+      if (value >= 50 && value <= 20000) {
+        return true;
+      } else {
+        throw new Error("El precio del producto debe estar entre $50 y $20000");
+      }
+    }),
+  //Aquí llamo a  resultadoValidacion
+  (req, res, next) => resultadoValidacion(req, res, next),
+];
 
-        export default validacionProducto;
+export default validacionProducto;
