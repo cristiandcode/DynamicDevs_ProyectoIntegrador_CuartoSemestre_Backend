e8964eeb43bae75b583e228f7a1507c740a4aaaa
Cristian <diazcristian0210@gmail.com>
2025-10-21 11:46:16 -0300

    agregue las validaciones al routes para hacer las pruebas con postman

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
-import { borrarProducto, crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
-const router = Router(); 
-
+import {
+  borrarProducto,
+  crearProducto,
+  editarProducto,
+  listarProductos,
+  obtenerProducto,
+} from "../controllers/productos.controllers.js";
+import validacionProducto from "../helpers/validacionProducto.js";
+const router = Router();
 //crear las rutas
-router.route('/prueba').get(listarProductos)
-router.route('/productos').post(crearProducto).get(listarProductos);
-router.route('/productos/:id').get(obtenerProducto).put(editarProducto).delete(borrarProducto)
+router.route("/prueba").get(listarProductos);
+router
+  .route("/productos")
+  .post([validacionProducto],crearProducto)
+  .get(listarProductos);
+router
+  .route("/productos/:id")
+  .get(obtenerProducto)
+  .put(editarProducto)
+  .delete(borrarProducto);
 export default router;
